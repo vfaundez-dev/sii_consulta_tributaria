638b2cd1ca4a73323961e29743f7f3d40346a99c
Vladimir Faundez Hernandez <v.faundezh@gmail.com>
2025-01-16 20:25:45 -0300

    Generado metodo para obtener datos tributarios en formato HTML y metodo para parsear datos y transformarlos a JSON

--- app/Http/Controllers/RequestSiiController.php
+++ app/Http/Controllers/RequestSiiController.php
@@ -17,19 +17,20 @@ class RequestSiiController extends Controller {
   public function fetchSiiData() {
 
 		$captcha = $this->fetchCaptcha();
-		if (!$captcha)
-			return $this->sendRequestMsg('error', 'Error obteniendo Captcha', []);
+		if (!$captcha) return $this->sendRequestMsg('error', 'Error obteniendo Captcha', []);
 
-		return $this->sendRequestMsg('success', 'Datos obtenidos', $captcha);
+		$dataSii = $this->fetchData('15417646', '2', $captcha);
+		$parsedData = $this->parseDataHtml($dataSii);
+
+		return $this->sendRequestMsg('success', 'Prueba completa', $parsedData);
     
   }
 
 	private function fetchCaptcha() {
     try {
 
-      $response = $this->client->post(
-        'https://zeus.sii.cl/cvc_cgi/stc/CViewCaptcha.cgi',[
-            'form_params' => ['oper' => 0]
+      $response = $this->client->post( 'https://zeus.sii.cl/cvc_cgi/stc/CViewCaptcha.cgi', [
+          'form_params' => ['oper' => 0]
         ]
       );
 
@@ -61,5 +62,93 @@ class RequestSiiController extends Controller {
 		]);
 	}
 
+	private function fetchData($rut, $dv, $captcha) {
+		try {
+
+			$postData = [
+				'RUT' => $rut,
+				'DV'  => $dv,
+				'PRG' => 'STC',
+				'OPC' => 'NOR',
+				'txt_code' => $captcha[0],
+				'txt_captcha' => $captcha[1]
+			];
+
+			$response = $this->client->post( 'https://zeus.sii.cl/cvc_cgi/stc/getstc', [
+          'form_params' => $postData
+        ]
+      );
+
+			$content = $response->getBody()->getContents();
+			return $content;
+
+		} catch (\GuzzleHttp\Exception\RequestException $e) {
+      // Registrar el error relacionado con la solicitud HTTP
+      Log::error("Error al realizar la solicitud: " . $e->getMessage(), [ 'exception' => $e ]);
+      return null;
+    } catch (\Exception $e) {
+      // Registrar cualquier otro tipo de error
+      Log::error("Ocurrió un error al procesar el captcha: " . $e->getMessage(), [ 'exception' => $e ]);
+      return null;
+    }
+	}
+
+	private function parseDataHtml($html) {
+		try {
+
+			$razonSocial = '';
+			$actividades = [];
+			$timbrados = [];
+
+			$documento = \phpQuery::newDocument($html);
+
+			// Extrae razón social
+			$razonSocialNode = $documento->find('body > div > div:nth-child(4)');
+			$razonSocial = $razonSocialNode->length > 0 
+					? ucwords(strtolower(trim($razonSocialNode->text())))
+					: null;
+
+			// Recorre las filas de la tabla actividades
+			$actividadRows = $documento->find('table.tabla')->eq(0)->find('tr');
+			foreach ($actividadRows as $i => $row) {
+				if ($i > 0) { // Ignora la cabecera
+					$rowNode = pq($row);
+					$actividades[] = [
+						'giro'      => trim($rowNode->find('td:nth-child(1) font')->text()),
+						'codigo'    => (int)trim($rowNode->find('td:nth-child(2) font')->text()),
+						'categoria' => trim($rowNode->find('td:nth-child(3) font')->text()),
+						'afecta'    => trim($rowNode->find('td:nth-child(4) font')->text()),
+						'fecha'     => trim($rowNode->find('td:nth-child(5) font')->text()),
+					];
+				}
+			}
+
+			// Recorre las filas de la tabla actividades
+			$timbradoRows = $documento->find('table.tabla')->eq(1)->find('tr');
+			foreach ($timbradoRows as $i => $row) {
+				if ($i > 0) { // Ignora la cabecera
+					$rowNode = pq($row);
+					$timbrados[] = [
+						'documento'             => trim($rowNode->find('td:nth-child(1) font')->text()),
+						'anio ultimo timbraje'  => (int)trim($rowNode->find('td:nth-child(2) font')->text()),
+					];
+				}
+			}
+
+			return [
+					'razonSocial' => $razonSocial,
+					'actividades' => $actividades,
+					'documentos timbrados' => $timbrados,
+			];
+
+		} catch (\Exception $e) {
+			Log::error('Error al procesar el HTML de respuesta', [
+				'error' => $e->getMessage(),
+				'trace' => $e->getTraceAsString()
+			]);
+
+			return null;
+		}
+	}
 
 }
