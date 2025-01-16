da538072d130848411a42c659857459c53cd6377
Vladimir Faundez Hernandez <v.faundezh@gmail.com>
2025-01-16 16:31:58 -0300

    Agregado controlador RequestSiiController y ruta api 'consulta'

---
+++ app/Http/Controllers/RequestSiiController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class RequestSiiController extends Controller {
+    
+    public function fetchSiiData() {
+
+
+
+    }
+
+}
