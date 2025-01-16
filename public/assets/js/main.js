3a189b057c2828ffc9a78ba83cece1d8515c4455
Vladimir Faundez Hernandez <v.faundezh@gmail.com>
2025-01-16 16:27:00 -0300

    Creada pagina principal. Agregado script y estilos main.css y main.js en carpeta public

---
+++ public/assets/js/main.js
@@ -0,0 +1,17 @@
+const form = document.getElementById('searchSII');
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const dataForm = new FormData(form);
+  getDataSII(dataForm);
+})
+
+async function getDataSII(dataForm) {
+  const dataSII = document.getElementById('dataSII');
+  const resp = await fetch('/api/search', {
+    method: 'POST',
+    body: dataForm
+  });
+  const data = await resp.json();
+  console.log(data);
+  dataSII.innerText = JSON.stringify(data, null, 2).trim();
+}
